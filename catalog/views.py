39ef9ca9c6276698acaacbd1e614348c68c8088a
Kurt <hell.trip@mail.ru>
2023-11-30 11:23:05 +0300

    complite_3 fix

--- catalog/views.py
+++ catalog/views.py
@@ -13,18 +13,25 @@ def home(request):
 
     return render(request, 'catalog/home.html', context)
 
+def product(request, id):
+    product_item = Product.objects.get(id=id)
+
+    context = {
+        'object_item': product_item,
+        'title': product_item.name
+    }
+    return render(request, 'catalog/product.html', context)
+
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        data = {'name': name, 'phone': phone, 'message': message}
-        with open('data.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False)
+    #if request.method == 'POST':
+    #    name = request.POST.get('name')
+    #    phone = request.POST.get('phone')
+    #    message = request.POST.get('message')
+    #    data = {'name': name, 'phone': phone, 'message': message}
+    #    with open('data.json', 'w', encoding='utf-8') as file:
+    #        json.dump(data, file, ensure_ascii=False)
 
     context = {
         'title': 'Контакты'
     }
-
-
     return render(request, 'catalog/contacts.html', context)
